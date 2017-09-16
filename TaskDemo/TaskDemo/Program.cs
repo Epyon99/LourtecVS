5a8191db1d8c8f0efe24b9cf693d62f3da7653ce
Moises <moises.chirinos@leuchterag.ch>
2017-09-15 19:24:06 -0500

    TASK DEMO: Ejemplo Async

---
+++ TaskDemo/TaskDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static void TaskTest()
+        {
+            System.Threading.CancellationToken x = new System.Threading.CancellationToken();
+
+            Task task1 = new Task(() => {
+                Console.WriteLine("Antes de Ejecutar 15 segundos");
+                Task.Delay(15000).Wait();
+                Console.WriteLine("Luego de Ejecutar 15 Segundos");
+            }
+            );
+            task1.Start();
+            var resultado = Task<string>.Run(() => {
+                Console.WriteLine("Antes de Ejecutar 2");
+                Task.Delay(10000).Wait();
+                Console.WriteLine("Luego de Ejecutar 2");
+                return "Hola Mundo";
+            }).ContinueWith((q) => {
+                Console.WriteLine("Antes de Ejecutar");
+                Task.Delay(10000).Wait();
+                Console.WriteLine("Luego de Ejecutar");
+                return q + " Hola";
+            });
+            resultado.Wait();
+            Console.WriteLine("Escribir por escribir");
+            Console.WriteLine($"Resultado:{resultado.Result}");
+
+            //task1.Start();
+            Console.ReadKey();
+        }
+    }
+}
