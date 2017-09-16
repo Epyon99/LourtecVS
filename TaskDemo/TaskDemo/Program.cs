69854cf57b7951855c45e821201e3d2e32352544
Moises <moises.chirinos@leuchterag.ch>
2017-09-15 20:14:56 -0500

    Async await

--- TaskDemo/TaskDemo/Program.cs
+++ TaskDemo/TaskDemo/Program.cs
@@ -1,35 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
-            
+            AsyncClass c = new AsyncClass();
+            c.SumaAsync(1, 2).Wait();
+        }
+
+        public static void ParallelAsync()
+        {
+
+        }
+
+        public async static void TaskAsync()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Antes de Ejecutar 15 segundos");
+                Console.WriteLine("Luego de Ejecutar 15 Segundos");
+            }
+            );
+        }
+
+        public static void ParallelTest()
+        {
+            while (true)
+            {
+                // Ciclo que se ejecuta cada 10 segundos
+                // Pueden aplicar System.Timers 
+                Task.Run(() => Console.WriteLine("x"));
+                Task.Delay(1).Wait();
+                break;
+            }
+
+            Parallel.Invoke(() =>
+            {
+                Console.WriteLine("1:" + DateTime.Now.Millisecond);
+                Task.Delay(new Random().Next(1000, 10000));
+                Console.WriteLine("Termino metodo 1:");
+            },
+                            () =>
+                            {
+                                Console.WriteLine("2:" + DateTime.Now.Millisecond);
+                                Task.Delay(new Random().Next(2000, 20000));
+                                Console.WriteLine("Termino metodo 2:");
+                            },
+                            () =>
+                            {
+                                Console.WriteLine("3:" + DateTime.Now.Millisecond);
+                                Task.Delay(new Random().Next(3000, 30000));
+                                Console.WriteLine("Termino metodo 3:");
+                            });
+            Console.ReadLine();
         }
 
         public static void TaskTest()
         {
             System.Threading.CancellationToken x = new System.Threading.CancellationToken();
 
-            Task task1 = new Task(() => {
+            Task task1 = new Task(() =>
+            {
                 Console.WriteLine("Antes de Ejecutar 15 segundos");
                 Task.Delay(15000).Wait();
                 Console.WriteLine("Luego de Ejecutar 15 Segundos");
             }
             );
             task1.Start();
-            var resultado = Task<string>.Run(() => {
+            var resultado = Task<string>.Run(() =>
+            {
                 Console.WriteLine("Antes de Ejecutar 2");
                 Task.Delay(10000).Wait();
                 Console.WriteLine("Luego de Ejecutar 2");
                 return "Hola Mundo";
-            }).ContinueWith((q) => {
+            }).ContinueWith((q) =>
+            {
                 Console.WriteLine("Antes de Ejecutar");
                 Task.Delay(10000).Wait();
                 Console.WriteLine("Luego de Ejecutar");
