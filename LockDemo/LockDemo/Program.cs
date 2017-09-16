239f2ca328374418063f22c799c4a77d7349fd00
Moises <moises.chirinos@leuchterag.ch>
2017-09-15 21:33:06 -0500

    Lock demo y Dispose Demo

---
+++ LockDemo/LockDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockDemo
+{
+    class Program
+    {
+        static readonly object _object = new object();
+
+        static void TestLock()
+        {
+            lock (_object)
+            {
+                Task.Delay(10000).Wait();
+                Console.WriteLine(DateTime.Now.TimeOfDay.ToString() + ":---" + DateTime.Now.Millisecond);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Run(() => TestLock());
+                Console.WriteLine($"For iteracion:{i}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
